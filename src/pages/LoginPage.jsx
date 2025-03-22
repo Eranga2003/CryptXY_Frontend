adf36347826cfe92966f2b9eff4a1f10288e15e3
Eranga Madushan <madushaneranga124@gmail.com>
2025-03-22 18:37:05 +0530

    add login page and add the logo

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import CryptX from "../componants/images/Crypt x (1).png";
+
+
 export default function LoginPage() {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <div className="bg-purple-950 h-screen flex justify-center items-center">
+  <div className="bg-white p-8 rounded-lg shadow-lg w-[650px] h-[650px] relative">
+    {/* Positioned Image Inside the Yellow Circle */}
+    <img
+      src={CryptX}
+      alt="CryptX Logo"
+      className="w-60 h-60 absolute top-[4%] left-[50%] transform -translate-x-1/2 rounded-lg"
+    />
+
+    {/* Large "Login" Heading */}
+    <h1 className="text-[80px]  text-gray-800 absolute top-[43%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 leading-none">
+      Login
+    </h1>
+  </div>
+</div>
+
     );
 }
